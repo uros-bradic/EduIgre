8da5201984fd913779e9d6e6e52034f4ef8bbd9a
uros-bradic <uros.bradic1975@gmail.com>
2020-10-13 00:23:11 +0200

    Some small changes

--- src/components/HomePage/CategoryList.tsx
+++ src/components/HomePage/CategoryList.tsx
@@ -3,6 +3,8 @@ import { Category } from "./Category";
 import { Categories } from "./Categories";
 
 export function CategoryList() {
-  const CategoryList = Categories.map((category) => <Category {...category} />);
+  const CategoryList = Categories.map((category, i) => (
+    <Category {...category} key={i} />
+  ));
   return <div className="categories">{CategoryList}</div>;
 }
